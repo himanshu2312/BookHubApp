631830b6c040af2c78be4adc3289c21ee14b7cbf
Himanshu_Gupta <himanshu2312gupta@gmail.com>
2023-01-26 18:20:37 +0530

    Fragments added and profile fragment Updated

--- app/src/main/java/com/himanshu/bookhub/MainActivity.kt
+++ app/src/main/java/com/himanshu/bookhub/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.FrameLayout
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 
@@ -15,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var frameLayout: FrameLayout
     private lateinit var navigationView: com.google.android.material.navigation.NavigationView
     private lateinit var toolbar: androidx.appcompat.widget.Toolbar
+    private var previousMenuItem: MenuItem?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,30 +33,82 @@ class MainActivity : AppCompatActivity() {
         )
         drawerLayout.setDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
+        openDashboard()
 
         navigationView.setNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.dashboard -> Toast.makeText(this@MainActivity,"Dashboard Clicked!!",Toast.LENGTH_SHORT).show()
-                R.id.profile -> Toast.makeText(this@MainActivity,"Profile Clicked!!",Toast.LENGTH_SHORT).show()
-                R.id.aboutApp -> Toast.makeText(this@MainActivity,"About AppClicked!!",Toast.LENGTH_SHORT).show()
-                R.id.favorite -> Toast.makeText(this@MainActivity,"Favorite Clicked!!",Toast.LENGTH_SHORT).show()
+            if (previousMenuItem!=null) previousMenuItem!!.isChecked=false
+            it.isChecked=true
+            when (it.itemId) {
+                R.id.dashboard -> openDashboard()
+                R.id.profile -> openProfile()
+                R.id.aboutApp -> openAboutApp()
+                R.id.favorite -> openFavorite()
             }
+            previousMenuItem=it
             return@setNavigationItemSelectedListener true
         }
     }
 
     private fun setUpToolbar() {
         setSupportActionBar(toolbar)
-        supportActionBar?.title = "Tool Bar"
+        supportActionBar?.title = "Dashboard"
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id=item.itemId
-        if (id==android.R.id.home){
+        val id = item.itemId
+        if (id == android.R.id.home) {
             drawerLayout.openDrawer(GravityCompat.START)
         }
         return super.onOptionsItemSelected(item)
     }
+
+    private fun openDashboard(){
+        val fragment=DashboardFragment()
+        val transaction=supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout,fragment)
+        transaction.commit()
+        supportActionBar?.title="Dashboard"
+        drawerLayout.closeDrawers()
+        navigationView.setCheckedItem(R.id.dashboard)
+    }
+
+    private fun openProfile(){
+        val fragment=ProfileFragment()
+        val transaction=supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout,fragment)
+        transaction.commit()
+        supportActionBar?.title="Profile"
+        drawerLayout.closeDrawers()
+    }
+
+    private fun openAboutApp(){
+        val fragment=AboutAppFragment()
+        val transaction=supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout,fragment)
+        transaction.commit()
+        supportActionBar?.title="About App"
+        drawerLayout.closeDrawers()
+    }
+
+    private fun openFavorite(){
+        val fragment=FavoriteFragment()
+        val transaction=supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout,fragment)
+        transaction.commit()
+        supportActionBar?.title="Favorite"
+        drawerLayout.closeDrawers()
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+
+        when(supportFragmentManager.findFragmentById(R.id.frameLayout)){
+            !is DashboardFragment -> openDashboard()
+            else -> super.onBackPressed()
+        }
+
+    }
+
 }
